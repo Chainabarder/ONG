c4473fbae682df60ee7601f1e571fd70b4a302f7
FELIX <felixshaw3031@gmail.com>
2017-09-29 10:58:52 -0300

    modifique cuestiones de estilo en todos los tabs // agregue al directorio un bloc de notas con info del hosteo // agregue el archivo .zip para la subida de archivos a la web // agregue el archivo jquery y agregue una carpeta de JS a Vision // POR ULTIMO AGREGUE 2 NUEVAS SECCIONES AL NAV --> NOSOTROS & CONTACTO --> ENTRE MISION Y VISION A UNA SOLA TAB

---
+++ tabs/Vision/js/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+        var imgs = document.querySelectorAll('img');
+        Array.prototype.forEach.call(imgs, function(el, i) {
+            if (el.tabIndex <= 0) el.tabIndex = 10000;
+        });
+    });
